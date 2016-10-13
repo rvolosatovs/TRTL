5b621a8edae752bc7f8848015b7413f6e8c857bf
Roman Volosatovs <rvolosatovs@riseup.net>
2016-10-13 10:23:58 +0200

    HWA9

--- trtl.ino
+++ trtl.ino
@@ -17,34 +17,57 @@ void WaitAndBlink( unsigned long DeltaMilliSec){
     } 
 }  
 
-Servo servoX;
-Servo servoY;
+Servo camera_servo_X;
+Servo camera_servo_Y;
+int camera_pos_X = 90;
+int camera_pos_Y = 90; 
+int camera_servo_X_pin = 5;
+int camera_servo_Y_pin = 6;
 
-// initial servo posititions in degrees
-int posY = 90; 
-int posX = 90;
+Servo grabber_arm_servo;
+Servo grabber_pull_servo;
+bool arm_lowered = true;
+bool puller_contracted = true;
+int grabber_arm_servo_pin= 3;
+int grabber_pull_servo_pin= 9;
+int GRABBER_PULL_SERVO_START = 60; 
+int GRABBER_PULL_SERVO_STOP = 120;
+int GRABBER_ARM_SERVO_START = 60; 
+int GRABBER_ARM_SERVO_STOP = 120;
 
-// arduino pins used to connect servo control signals
-int servoPinX = A0;
-int servoPinY = A1;
+int motor_A_enable = 10;
+int motor_A_direction_A = 4;
+int motor_A_direction_B = 7;
 
-// Pulse Width Modulation (PWM) pins
-int PWM1 = 3;
-int PWM2 = 5;
-int PWM3 = 6;
-int PWM4 = 11;
+int motor_B_enable = 11;
+int motor_B_direction_A = 12;
+int motor_B_direction_B = 13;
+
+// Motors = 10,11 PWM A/B
+// Directions = 4,7  mot. A
+// Directions = 12,13 mot. B
 
 void setup() {
-    analogWrite(PWM1,0);
-    analogWrite(PWM2,0);
-    analogWrite(PWM3,0);
-    analogWrite(PWM4,0);
+    camera_servo_X.attach(camera_servo_X_pin);
+    camera_servo_X.write(camera_pos_X);
 
-    servoX.attach(servoPinX);
-    servoY.attach(servoPinY);
+    camera_servo_Y.attach(camera_servo_Y_pin);
+    camera_servo_Y.write(camera_pos_Y);
 
-    servoX.write(posX);
-    servoY.write(posY);
+    grabber_arm_servo.attach(grabber_arm_servo_pin);
+    grabber_arm_servo.write(GRABBER_ARM_SERVO_START);
+
+    grabber_pull_servo.attach(grabber_pull_servo_pin);
+    grabber_pull_servo.write(GRABBER_PULL_SERVO_START);
+
+    digitalWrite(motor_A_enable, HIGH);
+    digitalWrite(motor_A_direction_A, LOW);
+    digitalWrite(motor_A_direction_B, LOW);
+
+    digitalWrite(motor_B_enable, HIGH);
+    digitalWrite(motor_B_direction_A, LOW);
+    digitalWrite(motor_B_direction_B, LOW);
+
 
     Serial.begin(9600); // in case of USB connection
     //Serial1.begin(115200); // in case of ethernet cable or WiFi
@@ -60,70 +83,91 @@ void setup() {
 }
 
 void loop() {
-    if(Serial.available() > 0)
-        //if(Serial1.available() > 0)
+    if(Serial.available() > 0) {
+        /*if(Serial1.available() > 0) {*/
 
-    {
         char Command = Serial.read();
         //char Command = Serial1.read();
+
         switch(Command) {
-            // Motors
+            /**
+             *   Motors
+             */
             case 'o':
                 // stop
-                analogWrite(PWM1,0);
-                analogWrite(PWM2,0);
-                analogWrite(PWM3,0);
-                analogWrite(PWM4,0);
+                digitalWrite(motor_A_direction_A, LOW);
+                digitalWrite(motor_A_direction_B, LOW);
+
+                digitalWrite(motor_B_direction_A, LOW);
+                digitalWrite(motor_B_direction_B, LOW);
                 break;
             case ';':
                 // backwards
-                analogWrite(PWM1,255);
-                analogWrite(PWM2,0);
-                analogWrite(PWM3,255);
-                analogWrite(PWM4,0);
+                digitalWrite(motor_A_direction_A, HIGH);
+                digitalWrite(motor_A_direction_B, LOW);
+
+                digitalWrite(motor_B_direction_A, LOW);
+                digitalWrite(motor_B_direction_B, HIGH);
                 break;
             case 'p':
                 // forwards
-                analogWrite(PWM1,0);
-                analogWrite(PWM2,255);
-                analogWrite(PWM3,0);
-                analogWrite(PWM4,255);
+                digitalWrite(motor_A_direction_A, LOW);
+                digitalWrite(motor_A_direction_B, HIGH);
+
+                digitalWrite(motor_B_direction_A, HIGH);
+                digitalWrite(motor_B_direction_B, LOW);
                 break;
             case 'l':
                 // left
-                analogWrite(PWM1,255);
-                analogWrite(PWM2,0);
-                analogWrite(PWM3,0);
-                analogWrite(PWM4,255);
+                digitalWrite(motor_A_direction_A, HIGH);
+                digitalWrite(motor_A_direction_B, LOW);
+
+                digitalWrite(motor_B_direction_A, HIGH);
+                digitalWrite(motor_B_direction_B, LOW);
                 break;  
             case '\'':
                 // right
-                analogWrite(PWM1,0);
-                analogWrite(PWM2,255);
-                analogWrite(PWM3,255);
-                analogWrite(PWM4,0);
+                digitalWrite(motor_A_direction_A, LOW);
+                digitalWrite(motor_A_direction_B, HIGH);
+
+                digitalWrite(motor_B_direction_A, LOW);
+                digitalWrite(motor_B_direction_B, HIGH);
                 break;
 
-                // Camera
+                /**
+                 *   Camera
+                 */
             case 'w':
                 // up
-                posY = posY + 5;
-                servoY.write(posY);
+                camera_pos_Y += 5;
+                camera_servo_Y.write(camera_pos_Y);
                 break;
             case 's':
                 // down
-                posY = posY - 5;
-                servoY.write(posY);
+                camera_pos_Y -= 5;
+                camera_servo_Y.write(camera_pos_Y);
                 break;
             case 'a':
                 // left
-                posX = posX + 5;
-                servoX.write(posX);
+                camera_pos_X += 5;
+                camera_servo_X.write(camera_pos_X);
                 break;
             case 'd':
                 // right
-                posX = posX - 5;
-                servoX.write(posX);
+                camera_pos_X -= 5;
+                camera_servo_X.write(camera_pos_X);
+                break;
+                /**
+                 *   Grabber
+                 */
+
+            case 'e':
+                grabber_arm_servo.write(GRABBER_ARM_SERVO_STOP);
+                grabber_pull_servo.write(GRABBER_PULL_SERVO_STOP);
+                break;
+            case 'r':
+                grabber_arm_servo.write(GRABBER_ARM_SERVO_START);
+                grabber_pull_servo.write(GRABBER_PULL_SERVO_START);
                 break;
         }
     }
